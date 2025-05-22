144aacd9724aa742ca0b728820abc48f70173901
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2025-05-22 09:23:48 -0400

    change TCP to NAT

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,23 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const validationResult = envVarsSchema.validate(process.env);
+const validationResult = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS
+    ? process.env.NATS_SERVERS.split(',')
+    : [],
+});
 const error = validationResult.error;
 const value = validationResult.value as EnvVars;
 
@@ -26,4 +33,5 @@ const envVars: EnvVars = value;
 export const envs = {
   PORT: envVars.PORT,
   DATABASE_URL: envVars.DATABASE_URL,
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
